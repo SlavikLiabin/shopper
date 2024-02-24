e691642608d998494f1f017007ef6a54cda179aa
Viacheslav Liabin <R9514633542@gmail.com>
2024-02-24 21:14:16 +0500

    'total'

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -23,9 +23,31 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+      return totalAmount;
+    }
+  };
 
-  const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+  const getTotalCartItems = () =>{
+    let totalItem = 0;
+    return 5
+  }
+
+  const contextValue = {
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <ShopContext.Provider value={contextValue}>
